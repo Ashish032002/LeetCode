a471e4a55192d6514592229ff7e0f0a0d094877e
Ashish Singh <90508026+Ashish032002@users.noreply.github.com>
2025-11-17 18:18:11 +0530

    Define address input structure for API
    Added a JSON structure for address input to the API.

--- Test.py
+++ Test.py
@@ -1,19 +1,9 @@
-Consider my project code,
-
-I want to make this project as api,  I need the endpoints so that I can raise a request to devops team for cluster access. So restructure my code folder accordingly so that I can push it to gitlab and raise cluster allocation request
-
-
-
-Basically what I wanted frop api is 
-
-   - Input will be a single address with address1, address2, address3, city, state, pincode, country fields and I'll validate  it using our logic and give .json format as output according the the demo.json file I've shared.
-
-   - The multiple address validation using .xl sheet we'll be doing locally not using the api so that remains same 
-
-
-
-And for product my database will be postgres in alloydb
-
-
-
-For api creation I want to use fastapi and for connecting with server I'll be using Cloud Run.
+{
+  "address1": "HSR Layout Sector 2",
+  "address2": "Near BDA Complex",
+  "address3": "Behind Temple",
+  "city": "Bangalore",
+  "state": "Karnataka",
+  "pincode": "560102",
+  "country": "India"
+}
