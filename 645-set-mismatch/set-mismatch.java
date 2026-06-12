fb84252f22f9e205daadc074dcdf7cf6626b4ff6
Ashish Singh <90508026+Ashish032002@users.noreply.github.com>
2026-06-12 19:30:08 +0530

    Time: 2 ms (95.05%) | Memory: 47.4 MB (77.49%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,42 +1,22 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] findErrorNums(int[] nums) {
+    int n = nums.length;
+    long Sn = (long) n * (n + 1) / 2;
+    long S2n = (long) n * (n + 1) * (2 * n + 1) / 6;
+    long S = 0; 
+    long S2 = 0;
 
-        Arrays.sort(nums);
-
-        int duplicate = -1;
-        int missing = -1;
-
-        // Find duplicate
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                duplicate = nums[i];
-                break;
-            }
-        }
-
-        // Find missing
-        if (nums[0] != 1) {
-            missing = 1;
-        } else {
-            for (int i = 0; i < nums.length - 1; i++) {
-
-                if (nums[i] == nums[i + 1]) {
-                    continue;
-                }
-
-                if (nums[i + 1] != nums[i] + 1) {
-                    missing = nums[i] + 1;
-                    break;
-                }
-            }
-
-            if (missing == -1) {
-                missing = nums[nums.length - 1] + 1;
-            }
-        }
-
-        return new int[]{duplicate, missing};
+    for(int i= 0 ;  i < nums.length ; i++){
+        S += nums[i];
+        S2 += (long)nums[i]*nums[i];
     }
+
+    long val1 = S-Sn;
+    long val2 = S2-S2n;
+    val2 = val2/val1;
+    long x = (val1 + val2) / 2;
+    long y = x - val1;
+
+    return new int[]{(int) x, (int) y};
+    }   
 }
