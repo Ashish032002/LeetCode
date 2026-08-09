7ac270eded2fc636f872981fef02672ad75cf7d5
Ashish Singh <90508026+Ashish032002@users.noreply.github.com>
2026-08-09 13:53:15 +0530

    Time: 7 ms (10.49%) | Memory: 46.7 MB (47.37%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Solution {
+
+    public List<List<String>> solveNQueens(int n) {
+
+        List<List<String>> ans = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+
+        solve(0, n, ans, current);
+
+        return ans;
+    }
+
+    public void solve(int row, int n,
+                      List<List<String>> ans,
+                      List<String> current) {
+
+        // All queens have been placed
+        if (row == n) {
+            ans.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (!isSafe(row, i, current)) {
+                continue;
+            }
+
+            char[] board = new char[n];
+            Arrays.fill(board, '.');
+
+            board[i] = 'Q';
+
+            current.add(new String(board));
+        
+            solve(row + 1, n, ans, current);
+
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public boolean isSafe(int row, int col,
+                           List<String> current) {
+
+        for (int r = 0; r < row; r++) {
+
+            String previousRow = current.get(r);
+
+            if (previousRow.charAt(col) == 'Q') {
+                return false;
+            }
+            int queenCol = previousRow.indexOf('Q');
+
+            if (Math.abs(r - row) == Math.abs(queenCol - col)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
